1c5ed9634ee44a39561abbb5ccee74dbfffd8559
Tobias Völkel <tobias.voelkel@st.ovgu.de>
2019-12-14 11:40:50 +0100

    amend scripts, change logging time

--- detectionQualityMap/gen_heatmap.py
+++ detectionQualityMap/gen_heatmap.py
@@ -3,22 +3,25 @@ import threading
 import numpy as np
 import time
 import atexit
-from std_msgs.msg import String
+from tracking_msgs.msg import TaggedPose2D
 
-downscaling_factor = 0.25
-heatmap = np.zeros(int(1600 * downscaling_factor), int(900 * downscaling_factor))
+downscaling_factor = 0.05
 new_calls = 0
 current_x = 0
 current_y = 0
+
+try:
+    heatmap = np.load("heatmap.npy")
+except:
+    heatmap = np.zeros((int(1600 * downscaling_factor), int(1200 * downscaling_factor)))
 
 def callback(data):
     global current_x
     global current_y
     global new_calls
-
-    print(data.data)
-    current_x = 0 # replace with value from topic
-    current_y = 0 # replace with value from topic
+	
+    current_x = int(data.x) # replace with value from topic
+    current_y = int(data.y) # replace with value from topic
     new_calls += 1
 
 def save_state():
@@ -27,11 +30,12 @@ def save_state():
     global current_y
     global heatmap
 
-    while(true):
+    while(True):
         heatmap[int(current_x * downscaling_factor), int(current_y * downscaling_factor)] = new_calls
-        print("Saved " + new_calls + " values at " + current_x + ", " + current_y)
+        np.save("heatmap", heatmap)
+        print("Saved " + str(new_calls) + " values at " + str(current_x) + ", " + str(current_y))
         new_calls = 0
-        time.sleep(1)
+        time.sleep(0.5)
 
 def save_heatmap():
     global heatmap
@@ -39,8 +43,10 @@ def save_heatmap():
 
 atexit.register(save_heatmap)
 
-heatmap_thread = threading.Thread(target = save_state())
+heatmap_thread = threading.Thread(target = save_state)
 heatmap_thread.start()
 
-subscriber = rospy.Subscriber("name", String, callback)
+print("Initializing subscriber")
+rospy.init_node("subscriber")
+subscriber = rospy.Subscriber("/nn_track0/eval/sphero_bright_blue", TaggedPose2D, callback)
 rospy.spin()
