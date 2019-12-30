c3a8ad63627c250a67cec53791b331d48de9ae76
wulfmeye <hans-martin.wulfmeyer@st.ovgu.de>
2019-12-30 20:45:26 +0100

    add tfrecords writing

--- Training Data Compositor/compositor.py
+++ Training Data Compositor/compositor.py
@@ -12,13 +12,18 @@ import csv
 import datetime
 from os import listdir, makedirs
 from os.path import isfile, join
-
+import io
+import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageEnhance
 from tqdm import tqdm
 import decimal
 
 np.random.seed(146324)
+
+object_classes = {
+    "sphero": 1
+}
 
 sphereo_classes = {
     "bright_blue": 1,
@@ -30,12 +35,30 @@ sphereo_classes = {
     "dark_red": 7
 }
 
+def int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def int64_list_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def bytes_list_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def float_list_feature(value):
+  return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 """
 - SHAPE = (height, width, 3)
 - the images have a random brightness
 """
-def creategaussiannoiseimg(SHAPE = (1200, 1600, 3), brtn = None):
+def generate_gaussiannoiseimg(SHAPE = (1200, 1600, 3), brtn = None):
     noise = np.random.randint(0, 255, SHAPE)
     noise = noise.astype(dtype=np.uint8)
     img = Image.fromarray(noise, mode='RGB')
@@ -48,81 +71,116 @@ def creategaussiannoiseimg(SHAPE = (1200, 1600, 3), brtn = None):
     return img
 
 
+def generate_random_image(img, bg, rot=None):
+    # random scale
+    scl = round(np.random.uniform(0.9, 1.1), 2)
+    img = img.resize((int(scl*img.width),int(scl*img.height)), resample=Image.LANCZOS)
+
+    # random rotation
+    if rot is None:
+        rot = np.random.randint(360)
+    img = img.rotate(rot, resample=Image.BICUBIC, expand=False)
+
+    # random brightness
+    brtn = round(np.random.uniform(0.5, 1.5), 2)
+    img = ImageEnhance.Brightness(img).enhance(brtn)
+
+    # random position
+    # !position is the upper left corner of the crop in the picture!
+    pos = (np.random.randint(0, bg.width-img.width), np.random.randint(0, bg.height-img.height))
+    bg.paste(img, pos, img)
+    return bg, img, pos
+
+
+def writecsv(filename, csv_rows):
+    with open(filename, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in csv_rows:
+            spamwriter.writerow(row)
+
+def writetfrecord(filename, tf_examples):
+    writer = tf.io.TFRecordWriter(filename)
+    for example in tf_examples:
+        writer.write(example.SerializeToString())
+    writer.close()
+
 """
 first stage: objection detection (to learn a bounding box for the objects)
 - random rgb noise backgrounds mit random brightness in Größe 300x300
 - auf die backgrounds werden die sphero crops superimposed
 - die crops haben eine random scale, rotation, brightness und position im background
 - die CSV datei enthält folgende spalten:
-	[image_name, xmin, xmax, ymin, ymax, img_class_str, img_class, rot, scl, brtn]
+	[image_name, width, height, object_class, xmin, xmax, ymin, ymax, img_class_str, img_class, rot, scl, brtn]
 	image_name: vollständige Name der Bilddatei
+    width, height: Weite und Höhe des bilds
+    object_class: objektklasse (z.b. 'sphero', 'copter', ...)
 	xmin, xmax, ymin, ymax: sind die koordinaten der bounding box
 	img_class_str: ist der name der klasse (siehe 'sphereo_classes' in compositor.py)
 	img_class: ist die ID der klasse (siehe 'sphereo_classes' in compositor.py)
 	rot: rotationswinkel
 	scl: scalingfaktor
 	brtn: brightnessfaktor
+TODO:
+    - mehrere objekte in einem bild ?
 """
 def firststage(isTrainingData):
-    TRAIN_SIZE = 1000
-    TEST_SIZE = 300
+    TRAIN_SIZE = 300
+    TEST_SIZE = 100
     BGHEIGHT = 300
     BGWIDTH = 300
-    FOLDER = "test/"
+    FOLDER = "evaluation"
     SIZE = TEST_SIZE
     if isTrainingData:
-        FOLDER = "training/"
+        FOLDER = "training"
         SIZE = TRAIN_SIZE
     OUT_PATH = "output/"
-    CROP_PATH = "crops/" + FOLDER
-    CROPS = [f for f in listdir(CROP_PATH) if isfile(join(CROP_PATH, f))]
-
+    CROP_PATH = "crops/" + FOLDER + "/"
     timestamp = "{:%Y%m%d_%H%M%S}".format(datetime.datetime.now())
     timestamp = "X"
-    IMG_OUT_PATH = OUT_PATH+"FirstStage_"+timestamp+"/"  + FOLDER
+    IMG_OUT_PATH = OUT_PATH+"FirstStage_"+timestamp+"/"  + FOLDER + "/"
+    TFREC_OUT_PATH = OUT_PATH+"FirstStage_"+timestamp+"/" + FOLDER
     makedirs(IMG_OUT_PATH, exist_ok=True)
+
+    CROPS = [f for f in listdir(CROP_PATH) if isfile(join(CROP_PATH, f))]
     csv_rows = []
+    tf_examples = []
 
     for i in tqdm(range(SIZE)):
-        bg = creategaussiannoiseimg(SHAPE = (BGHEIGHT, BGWIDTH, 3))
+        bg = generate_gaussiannoiseimg(SHAPE = (BGHEIGHT, BGWIDTH, 3))
         crop = np.random.choice(CROPS)
         img = Image.open(CROP_PATH + crop)
 
-        # random scale
-        scl = round(np.random.uniform(0.9, 1.1), 2)
-        img = img.resize((int(scl*img.width),int(scl*img.height)), resample=Image.LANCZOS)
+        bg, img, pos = generate_random_image(img, bg)
 
-        # random rotation
-        rot = np.random.randint(360)
-        img = img.rotate(rot, resample=Image.BICUBIC, expand=False)
-
-        # random brightness
-        brtn = round(np.random.uniform(0.5, 1.5), 2)
-        img = ImageEnhance.Brightness(img).enhance(brtn)
-
-        # random position
-        # !position is the upper left corner of the crop in the picture!
-        pos = (np.random.randint(0, bg.width-img.width), np.random.randint(0, bg.height-img.height))
-        
-        bg.paste(img, pos, img)
-
-        # save image and csv
-        img_class_str = crop[:-5]
-        img_class = sphereo_classes.get(img_class_str)
+        obj_class_str = "sphero"
+        obj_class = object_classes.get(obj_class_str)
 
         image_name = str(i) +  ".png"
         xmin = pos[0]
         xmax = xmin+img.width
         ymin = pos[1]
         ymax = ymin+img.height
-        csv_rows.append([image_name, xmin, xmax, ymin, ymax, img_class_str, img_class, rot, scl, brtn])
+        csv_rows.append([image_name, bg.width, bg.height, obj_class_str, obj_class, xmin, xmax, ymin, ymax])
+        tf_example = tf.train.Example(features=tf.train.Features(feature={      
+            'image/encoded': bytes_feature(bg.tobytes()),
+            'image/format': bytes_feature(b'png'),
+            'image/filename': bytes_feature(image_name.encode('utf8')),
+            'image/source_id': bytes_feature(image_name.encode('utf8')),
+            'image/width': int64_feature(bg.width),
+            'image/height': int64_feature(bg.height),
+            'image/object/class/text': bytes_feature(obj_class_str.encode('utf8')),
+            'image/object/class/label': int64_feature(obj_class),
+            'image/object/bbox/xmin': int64_feature(xmin),
+            'image/object/bbox/xmax': int64_feature(xmax),
+            'image/object/bbox/ymin': int64_feature(ymin),
+            'image/object/bbox/ymax': int64_feature(ymax),
+        }))
+        tf_examples.append(tf_example)
         bg.save(IMG_OUT_PATH+image_name)
 
-    with open(IMG_OUT_PATH+"groundtruth.csv", 'w') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in csv_rows:
-            spamwriter.writerow(row)
+    writecsv(IMG_OUT_PATH+"labels.csv", csv_rows)
+    writetfrecord(TFREC_OUT_PATH+".tfrecords", tf_examples)
 
 
 
@@ -133,10 +191,13 @@ second stage: Identification CNN and Orientation CNN
 
 - random rgb noise backgrounds mit random brightness in Größe 35x35
 - die crops haben die größe 30x30
-- crops bekommen eine random rotation, random scale, random helligkeit und random position im background
+- jeder crop wird zwei mal mit jedem winkel erstellt
+- zusätzlich erhalten die crops eine random scale, random helligkeit und random position im background
 - die CSV datei enthält folgende spalten:
-	[image_name, img_class_str, img_class, rot, scl, brtn, pos[0], pos[1]])
+	[image_name, width, heigth, object_class, img_class_str, img_class, rot, scl, brtn, pos[0], pos[1]])
 	image_name: vollständige Name der Bilddatei
+    width, height: Weite und Höhe des bilds
+    object_class: objektklasse (z.b. 'sphero')
 	img_class_str: ist der name der klasse (siehe 'sphereo_classes' in compositor.py)
 	img_class: ist die ID der klasse (siehe 'sphereo_classes' in compositor.py)
 	rot: rotationswinkel
@@ -150,75 +211,66 @@ TODO:
     - add noise to crops?
 """
 def secondstage(isTrainingData):
-    folder = "test/"
+
+    BGHEIGHT = 35
+    BGWIDTH = 35
+    FOLDER = "evaluation"
     if isTrainingData:
-        folder = "training/"
-
-
+        FOLDER = "training"
     OUT_PATH = "output/"
-    CROP_PATH = "crops/" + folder
-    #FIXHEIGHT = 30
-    #FIXWIDTH = 30
-
+    CROP_PATH = "crops/" + FOLDER + "/"
     timestamp = "{:%Y%m%d_%H%M%S}".format(datetime.datetime.now())
     timestamp = "X"
-    csv_rows = []
-    IMG_OUT_PATH = OUT_PATH+"SecondStage_"+timestamp+"/" + folder
+    IMG_OUT_PATH = OUT_PATH+"SecondStage_"+timestamp+"/"  + FOLDER + "/"
+    TFREC_OUT_PATH = OUT_PATH+"SecondStage_"+timestamp+"/" + FOLDER
     makedirs(IMG_OUT_PATH, exist_ok=True)
-
+    
     CROPS = [f for f in listdir(CROP_PATH) if isfile(join(CROP_PATH, f))]
+    csv_rows = []
+    tf_examples = []
+
     for crop in tqdm(CROPS):
-        for k in range(360*2):
-            img = Image.open(CROP_PATH + crop)
-            bg = creategaussiannoiseimg(SHAPE = (35,35,3))
+        for rot in range(360):
+            for k in range (2):
+                img = Image.open(CROP_PATH + crop)
+                bg = generate_gaussiannoiseimg(SHAPE = (BGHEIGHT, BGWIDTH, 3))
 
-            # random rotation
-            rot = np.random.randint(360)
-            img = img.rotate(rot, resample=Image.BICUBIC, expand=False)
+                bg, img, pos = generate_random_image(img, bg, rot)
 
-            """
-            #cropping
-            w, h = img.size
-            left = (w - FIXWIDTH)/2
-            top = (h - FIXHEIGHT)/2
-            right = (w + FIXWIDTH)/2
-            bottom = (h + FIXHEIGHT)/2
-            img = img.crop((left, top, right, bottom))
-            """
+                # save image and csv
+                obj_class_str = "sphero"
+                obj_class = object_classes.get(obj_class_str)
+                img_class_str = crop[:-5]
+                img_class = sphereo_classes.get(img_class_str)
+                image_name = img_class_str + crop[-5] + "_r" + str(rot) + "_" + str(k) + ".png"
+                bg.save(IMG_OUT_PATH+image_name)
+                csv_rows.append([image_name, bg.width, bg.height, img_class_str, img_class, rot, pos[0], pos[1]])
 
-            # random scale
-            scl = round(np.random.uniform(0.9, 1.1), 2)
-            img = img.resize((int(scl*img.width),int(scl*img.height)), resample=Image.LANCZOS)
+                tf_example = tf.train.Example(features=tf.train.Features(feature={      
+                    'image/encoded': bytes_feature(bg.tobytes()),
+                    'image/format': bytes_feature(b'png'),
+                    'image/filename': bytes_feature(image_name.encode('utf8')),
+                    'image/source_id': bytes_feature(image_name.encode('utf8')),
+                    'image/width': int64_feature(bg.width),
+                    'image/height': int64_feature(bg.height),
+                    'image/object/class/text': bytes_feature(obj_class_str.encode('utf8')),
+                    'image/object/class/label': int64_feature(obj_class),
+                    'image/object/subclass/text': bytes_feature(img_class_str.encode('utf8')),
+                    'image/object/subclass/label': int64_feature(img_class),
+                    'image/object/pose/orientation': int64_feature(rot),
 
-            # random brightness
-            brtn = round(np.random.uniform(0.5, 1.5), 2)
-            img = ImageEnhance.Brightness(img).enhance(brtn)
+                }))
+                tf_examples.append(tf_example)
 
-            # random position
-            # !position is the upper left corner of the crop in the picture!
-            pos = (np.random.randint(0, bg.width-img.width), np.random.randint(0, bg.height-img.height))
-            bg.paste(img, pos, img)
-
-            # save image and csv
-            img_class_str = crop[:-5]
-            img_class = sphereo_classes.get(img_class_str)
-            image_name = img_class_str + crop[-5] + "_r" + str(rot) + "_" + str(k) + ".png"
-            bg.save(IMG_OUT_PATH+image_name)
-            csv_rows.append([image_name, img_class_str, img_class, rot, scl, brtn, pos[0], pos[1]])
-
-
-    with open(IMG_OUT_PATH+"groundtruth.csv", 'w') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for row in csv_rows:
-            spamwriter.writerow(row)
+    writecsv(IMG_OUT_PATH+"labels.csv", csv_rows)
+    writetfrecord(TFREC_OUT_PATH+".tfrecords", tf_examples)
 
 
 if __name__ == "__main__":
-    img = creategaussiannoiseimg(SHAPE = (500, 500, 3), brtn=1.0)
+    img = generate_gaussiannoiseimg(SHAPE = (500, 500, 3), brtn=1.0)
     img.save("gaussian_noise.png")
 
-    #firststage(True)
-    #firststage(False)
+    firststage(True)
+    firststage(False)
     secondstage(True)
     secondstage(False)
